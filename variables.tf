6113295889b01079d8b3ece9e6e05d52abea169a
AlexPR704 <alexg97343@gmail.com>
2026-07-07 00:51:38 -0400

    Add full Terraform variable definitions for VPC, subnets, EC2, and environment configuration

--- variables.tf
+++ variables.tf
@@ -3,3 +3,39 @@ variable "aws_region" {
   type        = string
   default     = "us-east-1"
 }
+
+variable "project_name" {
+  description = "Name prefix for all resources"
+  type        = string
+  default     = "terraform-aws-env"
+}
+
+variable "environment" {
+  description = "Deployment environment (dev, staging, prod)"
+  type        = string
+  default     = "dev"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidrs" {
+  description = "CIDR blocks for public subnets"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "private_subnet_cidrs" {
+  description = "CIDR blocks for private subnets"
+  type        = list(string)
+  default     = ["10.0.3.0/24", "10.0.4.0/24"]
+}
+
+variable "ec2_instance_type" {
+  description = "EC2 instance type for the private VM"
+  type        = string
+  default     = "t3.micro"
+}
